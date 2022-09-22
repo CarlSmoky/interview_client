58d28544e6fd27c8fe645167ef3d9f7800594d65
tsumitakaoru <k.tsumita01@gmail.com>
2022-09-22 15:40:28 -0400

    change counter settings

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,31 +1,50 @@
 import React from 'react';
 
-const Form = ({ parameters, handleOnChange, onSave, defaultChecked, checkLimit }) => {
+const Form = ({ parameters, handleOnChange, start, defaultChecked, checkUnlimit, checked }) => {
+
+  const isChecked = (nameOfInput) => {
+    return checked[nameOfInput];
+  }
 
   return (
     <section className="section-configration">
-      <form onSubmit={onSave}>
-        <div className='config-box'>
-          <label className="config-box__label" htmlFor='numberOfQuestion'>Number of questions: </label>
-          <input className='config-box__input' name='numberOfQuestion' onChange={handleOnChange} value={parameters.numberOfQuestion} maxLength="4" size="4" />
+      <form onSubmit={start}>
+        <div className="setting-wrapper">
+          <div className="config-box">
+            <label className="config-box__label" htmlFor='numberOfQuestion'>Number of questions: </label>
+            <input className='config-box__input' type="number" name='numberOfQuestion' onChange={handleOnChange} value={parameters.numberOfQuestion} min="1" max="100"  />
+          </div>
         </div>
-        <div className='config-box'>
-          <input type="checkbox" id="limitPreparationTime" name="limitPreparationTime" value="limitPreparationTime" defaultChecked={defaultChecked.limitPreparationTime} onChange={checkLimit}/>
-          <label htmlFor="limitPreparationTime"> Limit preparation time</label>
+        <div className="setting-wrapper">
+          <h2 className="heading-small" >Preparation Counter</h2>
+          <div className="config-box">
+            <label className={`${isChecked('limitPreparationTime') ? "" : "dimmed"}`} htmlFor="limitPreparationTime">  Unlimited preparation time</label>
+            <input type="checkbox" id="limitPreparationTime" name="limitPreparationTime" value="limitPreparationTime" defaultChecked={defaultChecked.limitPreparationTime} onChange={checkUnlimit} />
+          </div>
+          <div className={`config-box ${isChecked('limitPreparationTime') ? "dimmed" : ""}`}>
+            <label className="config-box__label" htmlFor="preparingTime">Preparation time: </label>
+            <div className="time-box">
+              <input className='config-box__input' type="number" min="0" max="300" name='preparingTime' onChange={handleOnChange} value={parameters.preparingTime} maxLength="2" size="4" disabled={isChecked('limitPreparationTime')} />
+              <span className="unit">seconds</span>
+            </div>
+          </div>
+
         </div>
-        <div className='config-box'>
-          <label className="config-box__label" htmlFor="preparingTime">Preparation time: </label>
-          <input className='config-box__input' name='preparingTime' onChange={handleOnChange} value={parameters.preparingTime} maxLength="4" size="4" />
+        <div className="setting-wrapper">
+          <h2 className="heading-small" >Answer Counter</h2>
+          <div className="config-box">
+            <label className={`${isChecked('limitAnswaringTime') ? "" : "dimmed"}`} htmlFor="limitAnswaringTime"> Unlimited answaring time</label>
+            <input type="checkbox" id="limitAnswaringTime" name="limitAnswaringTime" value="limitAnswaringTime" defaultChecked={defaultChecked.limitAnswaringTime} onChange={checkUnlimit} />
+          </div>
+          <div className={`config-box ${isChecked('limitAnswaringTime') ? "dimmed" : ""}`}>
+            <label className="config-box__label" htmlFor="answeringTime">Answering time: </label>
+            <div className="time-box">
+              <input className='config-box__input' type="number" min="0" max="300" name='answeringTime' onChange={handleOnChange} value={parameters.answeringTime} maxLength="2" size="4" disabled={isChecked('limitAnswaringTime')} />
+              <span className="unit">seconds</span>
+            </div>
+          </div>
         </div>
-        <div className='config-box'>
-          <input type="checkbox" id="limitAnswaringTime" name="limitAnswaringTime" value="limitAnswaringTime" defaultChecked={defaultChecked.limitAnswaringTime} onChange={checkLimit}/>
-          <label htmlFor="limitAnswaringTime"> Limit answaring time</label>
-        </div>
-        <div className='config-box'>
-          <label className="config-box__label" htmlFor="answeringTime">Answering time: </label>
-          <input className='config-box__input' name='answeringTime' onChange={handleOnChange} value={parameters.answeringTime} maxLength="4" size="4" />
-        </div>
-        <button className="submit-button button" type="submit">Start</button>
+        <button className="submit-btn button" type="submit" >Start</button>
       </form>
     </section>
   )
--- src/components/SessionSettings.jsx
+++ src/components/SessionSettings.jsx
@@ -1,10 +1,10 @@
 import Form from './Form';
 
-const SessionSettings = ({handleOnChange, startSession, parameters, defaultChecked, checkLimit}) => {
+const SessionSettings = ({handleOnChange, startSession, parameters, defaultChecked, checkUnlimit, checked}) => {
   return (
     <section className="section-session_settings"> 
     <h1>Before Start</h1>
-    <Form handleOnChange={handleOnChange} onSave={startSession} parameters={parameters} defaultChecked={defaultChecked} checkLimit={checkLimit}/>
+    <Form handleOnChange={handleOnChange} start={startSession} parameters={parameters} defaultChecked={defaultChecked} checkUnlimit={checkUnlimit} checked={checked}/>
   </section>
   )
 }
